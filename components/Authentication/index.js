53a3d704fd1ab4aee5122274f87a50b0cf19444d
Skronkie <ebrockway01@gmail.com>
2022-06-20 15:07:27 -0500

    registration page

--- components/Authentication/index.js
+++ components/Authentication/index.js
@@ -1,30 +1,39 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import Login from "./components/Login"
-import Registration from "./components/Registration"
+import Login from "./components/Login";
+import Registration from "./components/Registration";
 
 export default function Authentication() {
-  const [typeOpen, setTypeOpen] = useState("registration")
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [loading, setLoading] = useState(false)
+  const [typeOpen, setTypeOpen] = useState("registration");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleUsernameChange = (text) => {
-    setUsername(text)
-  }
+    setUsername(text);
+  };
 
   const handlePasswordChange = (text) => {
-    setPassword(text)
-  }
+    setPassword(text);
+  };
 
-  const handleLoginSubmit = () => {
-    setLoading(true)
+  const handleEmailChange = (text) => {
+    setEmail(text);
+  };
+
+  const handleRegistrationSubmit = () => {
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-    }, 2500)
-    console.log("username", username)
-    console.log("password", password)
-  }
+      setLoading(false);
+    }, 2500);
+  };
+  const handleLoginSubmit = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  };
 
   return typeOpen === "login" ? (
     <Login
@@ -37,6 +46,16 @@ export default function Authentication() {
       submit={handleLoginSubmit}
     />
   ) : (
-    <Registration setTypeOpen={setTypeOpen} />
-  )
+    <Registration
+      setTypeOpen={setTypeOpen}
+      username={username}
+      password={password}
+      email={email}
+      handleEmailChange={handleEmailChange}
+      handleUsernameChange={handleUsernameChange}
+      handlePasswordChange={handlePasswordChange}
+      submit={handleRegistrationSubmit}
+      loading={loading}
+    />
+  );
 }
